ce512c3f40b7aaaf4d7b3e1fdbdfbc5d394d47f1
Robieh <10639273+RobiFox@users.noreply.github.com>
2024-04-26 14:38:22 +0200

    added colors, and using prefix instead of postfix

--- UltraSpeedRun/CheatsPatcher.cs
+++ UltraSpeedRun/CheatsPatcher.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 
 [HarmonyPatch(typeof(CheatsManager), "Start")]
 public class CheatsPatcher {
-    [HarmonyPostfix]
-    public static void Postfix(CheatsManager __instance) {
-        __instance.RebuildIcons();
+    [HarmonyPrefix]
+    public static void Prefix(CheatsManager __instance) {
+        //__instance.RebuildIcons();
         
-        Dictionary<string, List<ICheat>> registeredCheats = new Dictionary<string, List<ICheat>> { { "speedrun", new List<ICheat>() } };
-        registeredCheats["speedrun"].Add(new ShowZoneCheats());
-
-        foreach (KeyValuePair<string, List<ICheat>> kvp in registeredCheats) {
-            foreach (ICheat cheat in kvp.Value) {
-                __instance.RegisterCheat(cheat, kvp.Key);
-            }
-        }
-
-        CheatBinds.Instance.RestoreBinds(registeredCheats);
-        __instance.RebuildMenu();
+        Debug.Log("Yippe!");
+        __instance.RegisterExternalCheat(new ShowZoneCheats());
     }
 }
--- UltraSpeedRun/ShowZonesCheats.cs
+++ UltraSpeedRun/ShowZonesCheats.cs
@@ -1,44 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ShowZoneCheats : ICheat {
+    private bool _active = false;
 
     public string LongName => "Show All Trigger Zones";
 
     public string Identifier => "ultraspeedrun.zones";
 
-    public string ButtonEnabledOverride => "Don't Show Zones Next Restart";
+    public string ButtonEnabledOverride => "Hide Zones";
 
     public string ButtonDisabledOverride => "Show Zones";
 
     public string Icon => "noclip";
 
-    public bool IsActive => false;
+    public bool IsActive => _active;
 
     public bool DefaultState => false;
 
-    public StatePersistenceMode PersistenceMode => StatePersistenceMode.NotPersistent;
+    public StatePersistenceMode PersistenceMode => StatePersistenceMode.Persistent;
 
-    public void Disable()  {
+    private List<MeshRenderer> objectsMadeVisible = new List<MeshRenderer>();
+
+    public void Disable() {
+        _active = false;
+        foreach (var obj in objectsMadeVisible) {
+            obj.enabled = false;
+        }
     }
 
     public void Enable() {
-        theMaterial = SpeedRunMod.FindObjectOfType<PlayerActivator>().GetComponent<MeshRenderer>().material;
+        _active = true;
+        PlayerActivator activator = SpeedRunMod.FindObjectOfType<PlayerActivator>();
+        theMaterial = activator.GetComponent<MeshRenderer>().material;
+        theMesh = activator.GetComponent<MeshFilter>().mesh;
         SetObjectsVisible<ObjectActivator>();
         SetObjectsVisible<ActivateArena>();
+        SetObjectsVisible<DoorController>();
     }
 
     private Material theMaterial;
+    private Mesh theMesh;
+    
+    private Dictionary<Type, Color> _colors = new Dictionary<Type, Color>();
+    private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
 
+    public ShowZoneCheats() {
+        _colors.Add(typeof(ActivateArena), new Color(0.23f, 0.5f, 0.2f, 0.25f));
+        _colors.Add(typeof(DoorController), new Color(0.2f, 0.2f, 0.5f, 0.25f));
+    }
+    
     public void SetObjectsVisible<T>() where T : MonoBehaviour {
         foreach(var obj in Resources.FindObjectsOfTypeAll<T>()) {
             obj.gameObject.layer = 0;
             if(obj.TryGetComponent<MeshRenderer>(out MeshRenderer renderer)) {
-                renderer.enabled = true;
-                Debug.Log(theMaterial.name);
-                renderer.material = theMaterial;
+                MakeVisible<T>(renderer);
+            } else {
+                // IDK i will fix it later
+                MeshRenderer meshRenderer = obj.gameObject.AddComponent<MeshRenderer>();
+                MeshFilter meshFilter;
+                if (!obj.gameObject.TryGetComponent<MeshFilter>(out meshFilter)) {
+                    meshFilter = obj.gameObject.AddComponent<MeshFilter>();
+                }
+                meshFilter.mesh = theMesh;
+                MakeVisible<T>(meshRenderer);
             }
         }
+    }
+
+    public void MakeVisible<T>(MeshRenderer renderer) where T : MonoBehaviour {
+        renderer.enabled = true;
+        renderer.material = theMaterial;
+        if (_colors.ContainsKey(typeof(T))) {
+            //renderer.material.color = _colors[typeof(T)];
+            renderer.material.SetColor(EmissionColor, _colors[typeof(T)]);
+        }
+        if (!objectsMadeVisible.Contains(renderer)) objectsMadeVisible.Add(renderer);
     }
 
     public void Update() {
